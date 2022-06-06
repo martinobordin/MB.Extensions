8efec989ca9429e5d129d5683e1a066ce362488c
Martino Bordin <info@martinobordin.it>
2022-06-06 22:21:11 +0200

    Projects setup + versioning

---
+++ src/MB.Extensions/CollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace MB.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T>? source, Action<T> action)
+        {
+            if (source is null)
+            {
+                return;
+            }
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+
+        public static async Task ForEachAsync<T>(this IEnumerable<T>? source, Func<T, Task> action, CancellationToken cancellationToken = default)
+        {
+            if (source is null)
+            {
+                return;
+            }
+
+            foreach (var item in source)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                await action.Invoke(item).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ tests/MB.Extensions.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,29 @@
+namespace MB.Extensions.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        [Fact]
+        public void ForEach_WorksForNullException()
+        {
+
+        }
+
+        [Fact]
+        public void ForEach_WorksForNotNullException()
+        {
+
+        }
+
+        [Fact]
+        public void ForEachAsync_WorksForNullException()
+        {
+
+        }
+
+        [Fact]
+        public void ForEachAsync_WorksForNotNullException()
+        {
+
+        }
+    }
+}
