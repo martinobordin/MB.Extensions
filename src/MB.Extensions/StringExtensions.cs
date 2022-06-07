56377702d638e74d9e9a96fd9ef4d7cad491998e
Martino Bordin <info@martinobordin.it>
2022-06-07 12:57:31 +0200

    Added more extensions

--- src/MB.Extensions/StringExtensions.cs
+++ src/MB.Extensions/StringExtensions.cs
@@ -1,7 +1,21 @@
-﻿namespace MB.Extensions
+﻿using System;
+
+namespace MB.Extensions
 {
     public static class StringExtensions
     {
+        public static string Truncate(this string value, int maxlenght, string truncateChars = "")
+        {
+            if (string.IsNullOrWhiteSpace(value)) 
+            { 
+                return value; 
+            }
 
+            if (value.Length > maxlenght)
+            {
+                return string.Concat(value.AsSpan(0, maxlenght), truncateChars);
+            }
+            return value;
+        }
     }
 }
